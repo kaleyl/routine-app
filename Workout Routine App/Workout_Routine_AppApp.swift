b1661f84acedec239088e579dc207691390b9d8c
kaleyl <kkaleyleung@berkeley.edu>
2021-01-07 17:29:53 -0800

    Initial Commit

---
+++ Workout Routine App/Workout_Routine_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_Routine_AppApp.swift
+//  Workout Routine App
+//
+//  Created by Kaley Leung on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Workout_Routine_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
