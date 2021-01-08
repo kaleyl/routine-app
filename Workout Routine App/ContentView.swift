5afeb02a059cc46abe8e4e6171c53de94628f692
kaleyl <kkaleyleung@berkeley.edu>
2021-01-07 22:24:24 -0800

    import elegant calendar

--- Workout Routine App/ContentView.swift
+++ Workout Routine App/ContentView.swift
@@ -6,11 +6,19 @@
 //
 
 import SwiftUI
+import ElegantCalendar
+
+let startDate = Date().addingTimeInterval(TimeInterval(60 * 60 * 24 * (-30 * 36)))
+let endDate = Date().addingTimeInterval(TimeInterval(60 * 60 * 24 * (30 * 36)))
 
 struct ContentView: View {
+    
+    @ObservedObject var calendarManager = ElegantCalendarManager(
+        configuration: CalendarConfiguration(startDate: startDate, endDate: endDate))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ElegantCalendarView(calendarManager: calendarManager)
+        
     }
 }
 
